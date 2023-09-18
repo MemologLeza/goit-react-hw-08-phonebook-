58ba5727b185cfa45091072255a8c128cf292d1f
MemologLeza <lizabakay1@gmail.com>
2023-09-18 18:50:57 +0100

    update text mistakes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className="container mx-auto mt-5">
-      <h1 className="mb-3 fs-1">Wellcom dear customer 🤗</h1>
+      <h1 className="mb-3 fs-1">Welcome dear customer 🤗</h1>
       <p className="mb-3 fs-3">
         📞 "Phonebook" is a convenient online tool for managing your contacts.
         Easily add, delete, and filter your contacts securely.
